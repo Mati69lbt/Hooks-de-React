41e05d893ece5ba5babe5135440f8aad8c85ddab
Mati69lbt <mati69_lbt@hotmail.com>
2023-12-26 11:57:17 -0300

    useID

--- src/components/useid/UseId.jsx
+++ src/components/useid/UseId.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useId } from "react";
+import "./useid.css";
 
 const UseId = () => {
+  const id = useId();
+  const segundo_id = useId();
+
   return (
-    <div>
+    <div className="useID">
       <h1>useId</h1>
+      <input type="text" id={id} name="nombre" placeholder="Nombre" />
+      <p>
+        Nos sirve para crear un identidicado unico tanto para el cliente como
+        para el servidor
+      </p>
+      <p>Nos facilita crear tantos ide como querramos</p>
+      <pre>const id = useId()</pre>
+      <pre>
+        {`<input type="text" id={id} name="nombre" placeholder="nombre" />`}
+      </pre>
     </div>
   );
 };
